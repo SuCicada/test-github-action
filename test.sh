141a56e93d070715428c3ce3b46c928be5002a83
SuCicada <pengyifu@gmail.com>
2024-10-31 12:23:12 +0900

    test

--- test.sh
+++ test.sh
@@ -7,8 +7,9 @@ source ~/.bashrc
 # asdfsdfsa ad aasdfaasfdaasda a  a aasdf
 whoami
 env
-env-sync.rb download -y
-ls .env.test
-cat .env.test
+echo SUTOOLS $SUTOOLS
+# env-sync.rb download -y
+# ls .env.test
+# cat .env.test
 
-adfadfadsadfadf
+# adfadfadsadfadf
